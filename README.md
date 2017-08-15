4a2e604ec22d9429547cd686bde55fbed61df132
alexander.sobolev <alexander.sobolev@rokolabs.com>
2017-08-15 15:43:25 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# mparticle-android-integration-rokomobi
+## RokoMobi Kit Integration
+
+[See here for more information](https://github.com/mParticle/mparticle-android-sdk/wiki/Kit-Development) on how to use this example to write a new kit.
+
+This repository contains the [ROKO Mobi](https://roko.mobi/) integration for the [mParticle Android SDK](https://github.com/mParticle/mparticle-android-sdk).
+
+### Adding the integration
+
+1. Add the kit dependency to your app's build.gradle:
+
+    ```groovy
+    dependencies {
+        compile 'com.mparticle:android-rokomobi-kit:4+'
+    }
+    ```
+2. Follow the mParticle Android SDK [quick-start](https://github.com/mParticle/mparticle-android-sdk), then rebuild and launch your app, and verify that you see `"RokoMobi detected"` in the output of `adb logcat`.
+3. Reference mParticle's integration docs below to enable the integration.
+
+### Documentation
+
+[ROKO Mobi integration](https://docs.roko.mobi/v2.0/docs/android-basic-setup)
+
+### License
+
+[Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ src/test/java/com/mparticle/kits/KitTests.java
@@ -0,0 +1,40 @@
+package com.mparticle.kits;
+
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class KitTests {
+
+    private KitIntegration getKit() {
+        return new RokoMobiKit();
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+        String name = getKit().getName();
+        assertTrue(name != null && name.length() > 0);
+    }
+
+    @Test
+    public void testClassName() throws Exception {
+        KitIntegrationFactory factory = new KitIntegrationFactory();
+        Map<Integer, String> integrations = factory.getKnownIntegrations();
+        String className = getKit().getClass().getName();
+        for (Map.Entry<Integer, String> entry : integrations.entrySet()) {
+            if (entry.getValue().equals(className)) {
+                return;
+            }
+        }
+        fail(className + " not found as a known integration.");
+    }
+}
